b7ef665603740de1a2cf618fab2a159e32a1ade7
jackik <jackik1410@users.noreply.github.com>
2019-06-21 19:42:51 +0200

    !news now automatically pings <@&388765646554398734> in theory . . .

--- commands/news.js
+++ commands/news.js
@@ -1,4 +1,4 @@
-
+const Discord = require('discord.js');
 
 
 module.exports = {
@@ -12,17 +12,24 @@ module.exports = {
 
         }
         msg.delete(4000);
-        msg.reply('working on posting... (please wait a bit)').delete(5000)
+        msg.reply('working on posting... (please wait a bit)').then( m => m.delete(5000));
         var webhook = {
           "id":"590250045866115085",
           "token":"ar_Y5dJIWdGSt2ws327lQdSr3Wnoe-Tb9398gh1k8I2ksGnaVEAjmH7j85AoUni6ujwB"
         };
         var WEBHOOK = new Discord.WebhookClient(webhook.id, webhook.token);
 
-        WEBHOOK.send({
-          "name":"",
-          "color": 2735296,
-          "content":""
+        //var something = msg.content.slice(command.length).split('|');
+        var content = msg.content.slice(client.prefix.length + command.length +1).split('|');
+
+        WEBHOOK.send(`<@&388765646554398734>`,{
+          embeds:[
+            {
+            "title": content[0],
+            "color": 2735296,
+            "description": content[1]
+            }
+          ]
         })
           .catch(console.error);
 
