7a10477039e5bd93f2ecd31eeebb852dd5677e65
jackik <jackik1410@users.noreply.github.com>
2019-08-12 14:03:20 +0200

    added VWG triggers

--- commands/main-triggers-and-events.js
+++ commands/main-triggers-and-events.js
@@ -6,6 +6,34 @@ const winston = require(`../logger.js`).winston;
 const db = require(`../logger.js`).db;
 
 module.exports = {
+  "TriggerArray":[
+    {
+      "name":"VWG",
+      "shouldTrigger": async function shouldTrigger(client, msg){
+          if(msg.guild && msg.guild.id == '388765646554398734') return true;
+          return false;
+      },
+      "run": async function run(client, msg){
+          var keywords = {
+              "discord": {'react':'579419800837685298'},
+              "yuri": {'react':'579357609203728394'},
+              "slime":{'react':'579364833355497506'},
+              "bomb":{'react':'🍍'},
+              "sayori":{'react':'591314303026724916'},
+              "intel":{'react':'593081087178047511'},
+              "nvidia":{'react':'593122522476707842'},
+              "amd":{'react':'593080676014882829'}
+          }
+
+          Object.keys(keywords).forEach(async word =>{
+              if(msg.content.toLowerCase().match(word)) {
+                  console.log('found '+word+' in message');
+                  if(keywords[word].react) msg.react(`${keywords[word].react}`)
+              }
+          });
+      }
+    }
+  ],
   "events":[
     {
       "name":"ActivityUpdate",
