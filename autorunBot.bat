cd3e10e29302910e86b0d29e6e82aa5eeefd6cfa
jackik <jackik@users.noreply.github.com>
2019-09-13 22:34:23 +0200

    new autorun

--- autorunBot.bat
+++ autorunBot.bat
@@ -1,22 +1,31 @@
+set file=bot.js
 echo off
 cls
 echo %CD%
 #pause
 cls
 
+:choice
+choice /C idr /t 30 /d r /M "do you want to Install, Debug or just Run?"
+if %ERRORLEVEL% == 3 goto loop
+if %ERRORLEVEL% == 1 node install
+if %ERRORLEVEL% == 2 node inspect %file%
+goto choice
+
 :loop
+::I just prefer matrix-style terminals...
 color 0A
-:: I just prefer matrix-style terminals...
 cls
-call node bot.js
+call node %file%
 if %ERRORLEVEL% NEQ 0 ( ::pauses if didn't exit smoothly, recommended only if bot not on a remote machine
-color 0C
-echo.
-echo.
-echo Bot exited with error code %ERRORLEVEL%
-  pause
+    color 0C
+    echo.
+    echo.
+    echo App exited with error code %ERRORLEVEL%
+    pause
 )
 
-::timeout /t 1 /nobreak
+::timeout /t 1 /nobreak ::allows for timeouts before restarts. Recommended when on a remote machine and provides the opportunity to still see errors
+::use either the if or the timeout
 
 goto loop
